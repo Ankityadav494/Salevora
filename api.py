b7951a6248168ada9283d5fe68b524b52206577f
Ankityadav494 <ankityadavbkpur@gmail.com>
2026-04-12 15:06:07 +0530

    Cleaned project for deployment

--- api.py
+++ api.py
@@ -439,4 +439,4 @@ async def inv_ws(websocket: WebSocket):
 # ------------------------------------------------------------------ #
 if __name__ == "__main__":
     uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
- 
+
